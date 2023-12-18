77adeed7d123d99306ddc4907870fc2a7a222d5a
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-18 22:43:30 +0400

    Update raspisaniye.java

--- raspisaniye.java
+++ raspisaniye.java
@@ -1,34 +1,34 @@
   import java.util.Scanner;
-      public class raspisaniye {
-        public static void main(String[] arg) {
-Scanner sc=new Scanner(System.in);
-System.out.println("Viberi den nedeli:");
-System.out.println("1.Ponedelnik");
-System.out.println("2.Vtornik");
-System.out.println("3.Sreda");
-System.out.println("4.Chetverq");
-System.out.println("5.Pyatnitsa");
-int x=sc.nextInt();
-switch(x){
-default:
-System.out.println("Vibrano neverno");
-break;
-case 1:
-System.out.println("Russkiy, Matematika, Chimiya, Fizkultura");
-break;
-case 2:
-System.out.println("Fizika, Matematika, Biologiya, Literatura");
-break;
-case 3:
-System.out.println("Matematika, Geoqrafiya, Russkiy, Texnologiya");
-break;
-case 4:
-System.out.println("Chercheniye, Izo, Muzika, Matematika");
-break;
-case 5:
-System.out.println("Russkiy, Literatura, Matematika, Informatika");
-break;
+  public class raspisaniye {
+    public static void main(String[] arg) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Select day of week:");
+      System.out.println("1.Monday");
+      System.out.println("2.Tuesday");
+      System.out.println("3.Wednesday");
+      System.out.println("4.Thursday");
+      System.out.println("5.Friday");
+      int x = sc.nextInt();
+      switch (x) {
+      default:
+        System.out.println("selected incorrectly");
+        break;
+      case 1:
+        System.out.println("Russian language, mathematics, chemistry, physical training");
+        break;
+      case 2:
+        System.out.println("physics, mathematics, biology, literature");
+        break;
+      case 3:
+        System.out.println("mathematics, geography, russian language, technology");
+        break;
+      case 4:
+        System.out.println("drawing, art, music, mathematics");
+        break;
+      case 5:
+        System.out.println("russian language, literature, mathematics, informatics");
+        break;
 
-}
-}
-}
+      }
+    }
+  }
